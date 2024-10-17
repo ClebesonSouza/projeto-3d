b5d863868817403c4827f89f04eabec7bb64da30
Clebeson Souza <klebesonlucas@gmail.com>
2024-10-17 17:55:59 -0300

    publicação do site 3d

---
+++ script.js
@@ -0,0 +1,44 @@
+const navs = document.querySelectorAll('.nav-list li');
+const cube = document.querySelector('.box');
+
+
+
+const resumeLists = document.querySelectorAll('.resume-list');
+const resumeBoxs = document.querySelectorAll('.resume-box');
+
+const portfolioLists = document.querySelectorAll('.portfolio-list');
+const portfolioBoxs = document.querySelectorAll('.portfolio-box');
+
+//navbar actions and all section actions along with cube rotation when navbar is clicked
+navs.forEach((nav, idx) => {
+    nav.addEventListener('click', () => {
+        document.querySelector('.nav-list li.active').classList.remove('active');
+           nav.classList.add('active');
+
+           cube.style.transform = 'rotateY(${idx * -90}deg)';
+    });
+});
+
+resumeLists.forEach((list, idx) => {
+    list.addEventListener('click', () => {
+        document.querySelector('.resume-list.active').classList.remove('active');
+           list.classList.add('active');
+
+        document.querySelector('.resume-box.active').classList.remove('active');
+        resumeBoxs[idx].classList.add('active');
+    });
+});
+
+//portfolio section when clicking tab-list
+
+portfolioLists.forEach((list, idx) => {
+    list.addEventListener('click', () => {
+        document.querySelector('.portfolio-list.active').classList.remove('active');
+           list.classList.add('active');
+
+           document.querySelector('.portfolio-box.active').classList.remove('active');
+           portfolioBoxs[idx].classList.add('active');
+    });
+});
+
+//visibility for contact section when reLoading (cube reLoading animation)
